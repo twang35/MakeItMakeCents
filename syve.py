72b8b15d53cca93e40c44e459ebbfc9aaf863a3c
Tony Wang <twang35@users.noreply.github.com>
2024-02-02 11:44:58 +0700

    reformat syve.py

--- syve.py
+++ syve.py
@@ -2,7 +2,8 @@ import requests
 
 with open('syve_key.txt', 'r') as f:
     syve_key = f.read()
-url = f"https://api.syve.ai/v1/filter-api/token-balances?key={syve_key}&gte:block_number=16000000&lte:block_number=17000000&size=10&sort=desc"
+url = (f"https://api.syve.ai/v1/filter-api/token-balances?key={syve_key}&gte:block_number=16000000&lte:block_number"
+       f"=17000000&size=10&sort=desc")
 
 response = requests.get(url)
 
