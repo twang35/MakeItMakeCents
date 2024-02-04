2edefb79d9c85dc87548764a1ad121dcd9f42825
Tony Wang <twang35@users.noreply.github.com>
2024-02-04 07:30:29 +0700

    add sql query to svye

--- syve.py
+++ syve.py
@@ -2,9 +2,13 @@ import requests
 
 with open('syve_key.txt', 'r') as f:
     syve_key = f.read()
-url = (f"https://api.syve.ai/v1/filter-api/token-balances?key={syve_key}&gte:block_number=16000000&lte:block_number"
-       f"=17000000&size=10&sort=desc")
+url = (f"https://api.syve.ai/v1/sql?key={syve_key}")
+query = ("select * from eth_token_balances where token_address = '0x111111111117dc0aa78b770fa6a738034120c302' order by "
+         "balance_token desc limit 50")
+# query = "SELECT pool_address FROM eth_dex_trades WHERE token_address = '0x111111111117dc0aa78b770fa6a738034120c302' LIMIT 10"
+data = {'query': query}
 
-response = requests.get(url)
+response = requests.post(url, json=data)
+
 
 print(response.text)
