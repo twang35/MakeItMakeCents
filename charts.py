811eca858b066c08344c9d34a353e69d637749af
Tony Wang <twang35@users.noreply.github.com>
2024-02-15 10:25:54 +0700

    make the largest realized gains charts more generic and add filter_out_addresses

--- charts.py
+++ charts.py
@@ -6,14 +6,27 @@ from plotly.subplots import make_subplots
 from database import *
 import time
 
+filter_out_addresses = [
+    '0x3aF2ACB662A241da4ef4310C7AB226f552B42115', # Altlayer Airdrop Safe Smart Account
+    '0x9c7F0628ceE619953Fc395Cd7cF0576DCe1F505E', # PepeFork Airdrop contract
+]
+
 
 def charts():
     conn = create_connection()
+    cursor = conn.cursor()
 
-    balances = load_balances(conn, altlayer_token_address)
-    balances_map = compute_balances_map(balances)
-    prices = load_prices(conn, altlayer_token_address)
+    token_address = altlayer_token_address
+    # token_address = pepefork_token_address
 
+    balances_rows = get_largest_balances(cursor, token_address)
+    balances_map = compute_balances_map(balances_rows)
+    prices = load_prices(cursor, token_address)
+
+    create_realized_gains_graph(prices, balances_map)
+
+
+def create_realized_gains_graph(prices, balances_map):
     # 0 price, 1 timestamp
     prices_y = [row[0] for row in prices]
     prices_timestamp = [row[1] for row in prices]
@@ -38,46 +51,10 @@ def charts():
     fig.show()
 
 
-def load_balances(conn, token_address):
-    cursor = conn.cursor()
-
-    start_time = time.time()
-    query = f"""
-        SELECT 
-            wallet_address, 
-            timestamp,
-            balance, 
-            total_cost_basis,
-            remaining_cost_basis,
-            realized_gains
-        FROM 
-            balances
-        WHERE 
-            wallet_address in ('0xbd513f67ed9bccc8364176e8b97bb93a5030e777',
-                                '0x28c6c06298d514db089934071355e5743bf21d60',
-                                '0xdfd5293d8e347dfe59e90efd55b2956a1343963d',
-                                '0x21a31ee1afc51d94c2efccaa2092ad1028285549',
-                                '0x0d4d42969105e5a144a2538aeb24a2e16be40e34',
-                                '0xc882b111a75c0c657fc507c04fbfcd2cc984f071',
-                                '0xecd3b4b7fd0bc9f52335f29a3e007453de07d2b9',
-                                '0x58edf78281334335effa23101bbe3371b6a36a51',
-                                '0x0d0707963952f2fba59dd06f2b425ace40b492fe',
-                                '0xfcc43eb62b4da4bc5d2260721a497c4693e59a70')
-            AND token_address='{token_address}'
-        ORDER BY
-            timestamp;
-        """
-    cursor.execute(query)
-    balances = cursor.fetchall()
-    print("Total balances rows are:  ", len(balances))
-    print(f'query time: {time.time() - start_time}')
-    return balances
-
-
-def compute_balances_map(balances):
+def compute_balances_map(balances_rows):
     # 0 wallet_address, 1 timestamp, 2 balance, 3 total_cost_basis, 4 remaining_cost_basis, 5 realized_gains
     balances_map = {}
-    for row in balances:
+    for row in balances_rows:
         if row[0] not in balances_map:
             balances_map[row[0]] = [[], []]
         balances_map[row[0]][0].append(row[5])  # realized_gains
@@ -86,9 +63,7 @@ def compute_balances_map(balances):
     return balances_map
 
 
-def load_prices(conn, token_address):
-    cursor = conn.cursor()
-
+def load_prices(cursor, token_address):
     start_time = time.time()
     query = f"""
         SELECT 
@@ -106,6 +81,78 @@ def load_prices(conn, token_address):
     print("Total prices rows are:  ", len(prices))
     print(f'query time: {time.time() - start_time}')
     return prices
+
+
+def get_largest_balances(cursor, token_address):
+    largest_wallets = get_largest_wallets(cursor, token_address)
+    return load_balances(cursor, token_address, largest_wallets)
+
+
+def get_largest_wallets(cursor, token_address):
+    start_time = time.time()
+    find_largest_wallets_query = f"""
+        SELECT * 
+        FROM (
+            SELECT 
+              wallet_address, 
+              timestamp, 
+              balance, 
+              total_cost_basis, 
+              remaining_cost_basis, 
+              realized_gains, 
+              ROW_NUMBER() OVER (
+                PARTITION BY wallet_address 
+                ORDER BY 
+                  block_number DESC
+              ) AS row_num 
+            FROM 
+              balances 
+            WHERE 
+              token_address = '{token_address}'
+              AND wallet_address NOT IN ({to_sql_collection_string(filter_out_addresses)})
+        ) AS ranked 
+        WHERE 
+            row_num = 1 
+        ORDER BY 
+            realized_gains DESC
+        LIMIT 15;
+        """
+    cursor.execute(find_largest_wallets_query)
+    largest_wallets_rows = cursor.fetchall()
+    print(f'find_largest_wallets time: {time.time() - start_time}')
+    return [row[0] for row in largest_wallets_rows]
+
+
+def load_balances(cursor, token_address, largest_wallets):
+    start_time = time.time()
+    wallet_addresses = to_sql_collection_string(largest_wallets)
+    load_wallets_balances_query = f"""
+        SELECT 
+            wallet_address, 
+            timestamp,
+            balance, 
+            total_cost_basis,
+            remaining_cost_basis,
+            realized_gains
+        FROM 
+            balances
+        WHERE 
+            wallet_address IN ({wallet_addresses})
+            AND token_address='{token_address}'
+        ORDER BY
+            block_number;
+        """
+    cursor.execute(load_wallets_balances_query)
+    balances_rows = cursor.fetchall()
+    print(f'load_wallets_balances time: {time.time() - start_time}')
+    print("Total get_largest_balances rows:  ", len(balances_rows))
+    return balances_rows
+
+
+def to_sql_collection_string(items):
+    result = [f"'{item.lower()}',\n" for item in items]
+    result[-1] = result[-1][:-2]  # remove last \n and , from last wallet_address
+    return " ".join(result)
 
 
 charts()
