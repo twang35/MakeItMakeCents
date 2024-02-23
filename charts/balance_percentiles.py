2bde6939b419553b9a0b5a6412dc5ab929b33228
Tony Wang <twang35@users.noreply.github.com>
2024-02-23 09:07:02 +0800

    add example of multiple graphs, no way yet to add shared x spike

--- charts/balance_percentiles.py
+++ charts/balance_percentiles.py
@@ -7,6 +7,25 @@ from database import *
 
 def balance_percentiles():
     print('balance_percentiles')
+    fig = make_subplots(
+        rows=5, cols=2,
+        specs=[[{"rowspan": 2, "colspan": 2}, None],
+               [None, None],
+               [{}, {"rowspan": 2}],
+               [{}, None],
+               [{}, {}]],
+        print_grid=True)
+
+    fig.add_trace(go.Scatter(x=[1, 2], y=[1, 2], name="(1,1)"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=[1, 2], y=[1, 2], name="(3,1)"), row=3, col=1)
+    fig.add_trace(go.Scatter(x=[1, 2], y=[1, 2], name="(3,2)"), row=3, col=2)
+    fig.add_trace(go.Scatter(x=[1, 2], y=[1, 2], name="(4,1)"), row=4, col=1)
+    fig.add_trace(go.Scatter(x=[1, 2], y=[1, 2], name="(5,1)"), row=5, col=1)
+    fig.add_trace(go.Scatter(x=[1, 2], y=[1, 2], name="(5,2)"), row=5, col=2)
+
+    fig.update_layout(title_text="specs examples")
+    fig.update_layout(hovermode="x unified")
+    fig.show()
     print(BalancesColumns.wallet_address)
 
 
