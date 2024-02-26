6866779a861a89faa512169387214f413751143e
Tony Wang <twang35@users.noreply.github.com>
2024-02-26 13:29:56 +0700

    add token name to print logs

--- update.py
+++ update.py
@@ -15,7 +15,10 @@ def update():
     # only_print_queue(queue='update')
 
     # Update existing token #########################
-    update_all_data(mubi.address)
+    update_all_data(altlayer)
+    update_all_data(pepefork)
+    update_all_data(xcad)
+    update_all_data(mubi)
 
     # Download new token #########################
     # token_address = mubi.address
@@ -26,56 +29,56 @@ def update():
     # update_all_data(token_address, new_token=True, start_txn_block=start_txn_block, start_price_time=start_price_time)
 
 
-def update_all_data(token_address, new_token=False, start_txn_block=None, start_price_time=None):
+def update_all_data(token, new_token=False, start_txn_block=None, start_price_time=None):
     queue_name = 'update'
     conn = create_connection()
     web3 = Web3(HTTPProvider(ankr_endpoint))
 
     if not new_token:
-        start_txn_block = get_latest_transactions_block(conn, token_address)
-        start_price_time = get_latest_price_timestamp(conn, token_address)
+        start_txn_block = get_latest_transactions_block(conn, token.address)
+        start_price_time = get_latest_price_timestamp(conn, token.address)
 
     # update for new tokens if the start_txn_block is before the earliest block in block_times
     if new_token:
-        print('====== start updating block_times before earliest block_times block =============')
+        print(f'====== start updating block_times before earliest block_times block for {token.name} =============')
         earliest_block_times_block = get_earliest_block_times_block(conn)
         add_items_to_queue(start=start_txn_block,
                            end=earliest_block_times_block, queue=queue_name,
                            increment=batch_block_time_increment)
         process_blocks(queue='update', table='block_times', max_block_num=earliest_block_times_block)
-        print('====== completed updating block_times before earliest block_times block =========')
+        print(f'====== completed updating block_times before earliest block_times block for {token.name} =========')
 
     latest_finalized_block = web3.eth.get_block(block_identifier='finalized')['number']
 
     # update block_times until latest finalized block
-    print('====== start updating block_times =============')
+    print(f'====== start updating block_times for {token.name} =============')
     add_items_to_queue(start=get_latest_block_times_block(conn),
                        end=latest_finalized_block, queue=queue_name,
                        increment=batch_block_time_increment)
     process_blocks(queue='update', table='block_times', max_block_num=latest_finalized_block)
-    print('====== completed updating block_times =========')
+    print(f'====== completed updating block_times for {token.name} =========')
 
     # update transactions, uses block_times to populate timestamp field
-    print('====== start updating transactions ============')
+    print(f'====== start updating transactions for {token.name} ============')
     add_items_to_queue(start=start_txn_block,
                        end=latest_finalized_block, queue=queue_name,
                        increment=batch_txn_logs_increment)
     process_blocks(queue=queue_name, table='transactions', max_block_num=latest_finalized_block,
-                   token_address=token_address)
-    print('====== completed updating transactions ========')
+                   token_address=token.address)
+    print(f'====== completed updating transactions for {token.name} ========')
 
     # update prices
-    print('====== start updating prices ==================')
+    print(f'====== start updating prices for {token.name} ==================')
     add_items_to_queue(start=start_price_time,
                        end=datetime.datetime.utcnow(), queue=queue_name,
                        increment=batch_price_date_increment)
-    process_price_dates(token_address=token_address, queue_name=queue_name)
-    print('====== completed updating prices ==============')
+    process_price_dates(token_address=token.address, queue_name=queue_name)
+    print(f'====== completed updating prices for {token.name} ==============')
 
     # update balances
-    print('====== start updating balances ================')
-    compute_balances(conn, token_address)
-    print('====== completed updating balances ============')
+    print(f'====== start updating balances for {token.name} ================')
+    compute_balances(conn, token.address)
+    print(f'====== completed updating balances for {token.name} ============')
 
 
 if __name__ == "__main__":
